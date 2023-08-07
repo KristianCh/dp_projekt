7cfb779c2f292501fbe2b6411cea94f48aae6f84
Kristián Chovančák <chovancak.kristian@gmail.com>
2023-08-07 18:38:44 +0200

    Added signals

---
+++ Assets/Entities/Events/ASignal.cs
@@ -0,0 +1,64 @@
+﻿// // Author: Kristián Chovančák
+// // Created: 21.07.2023
+// // Copyright (c) Noxgames
+// // http://www.noxgames.com/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Entities.Events
+{
+    public class ASignal
+    {
+        public event Action<ASignal, object[]> BaseListener;
+
+        public event Action<ASignal, object[]> OnceBaseListener;
+
+        public void Dispatch(object[] args)
+        {
+            if (BaseListener != null)
+                BaseListener(this, args);
+            if (OnceBaseListener != null)
+                OnceBaseListener(this, args);
+            OnceBaseListener = null;
+        }
+
+        public virtual List<Type> GetTypes()
+        {
+            return new List<Type>();
+        }
+
+        public void AddListener(Action<ASignal, object[]> callback)
+        {
+            BaseListener = AddUnique(BaseListener, callback);
+        }
+
+        public void AddOnce(Action<ASignal, object[]> callback)
+        {
+            OnceBaseListener = AddUnique(OnceBaseListener, callback);
+        }
+
+        private Action<T, U> AddUnique<T, U>(Action<T, U> listeners, Action<T, U> callback)
+        {
+            if (listeners == null || !listeners.GetInvocationList().Contains(callback))
+            {
+                listeners += callback;
+            }
+
+            return listeners;
+        }
+        
+        public void RemoveListener(Action<ASignal, object[]> callback)
+        {
+            if (BaseListener != null)
+                BaseListener -= callback;
+        }
+        
+        public virtual void RemoveAllListeners()
+        {
+            BaseListener = null;
+            OnceBaseListener = null;
+        }
+    }
+}
---
+++ Assets/Entities/Events/Signal.cs
@@ -0,0 +1,122 @@
+﻿// // Author: Kristián Chovančák
+// // Created: 21.07.2023
+// // Copyright (c) Noxgames
+// // http://www.noxgames.com/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Entities.Events
+{
+        public class Signal : ASignal
+    {
+        public event Action Listener = null;
+        public event Action OnceListener = null;
+
+        public void AddListener(Action callback)
+        {
+            Listener = this.AddUnique(Listener, callback);
+        }
+
+        public void AddOnce(Action callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+
+        public void RemoveListener(Action callback)
+        {
+            if (Listener != null)
+                Listener -= callback;
+        }
+
+        public void RemoveOnceListener(Action callback)
+        {
+            if (OnceListener != null)
+                OnceListener -= callback;
+        }
+
+        public override List<Type> GetTypes() { return new List<Type>(); }
+
+        public virtual void Dispatch()
+        {
+            Listener?.Invoke();
+            OnceListener?.Invoke();
+            OnceListener = null;
+            base.Dispatch(null);
+        }
+
+        protected Action AddUnique(Action listeners, Action callback)
+        {
+            if (listeners == null || !listeners.GetInvocationList().Contains(callback))
+            {
+                listeners += callback;
+            }
+
+            return listeners;
+        }
+
+        public override void RemoveAllListeners()
+        {
+            Listener = null;
+            OnceListener = null;
+            base.RemoveAllListeners();
+        }
+
+        public Delegate listener { get { return Listener ?? (Listener = delegate { }); } set { Listener = (Action) value; } }
+    }
+
+    public class Signal<T> : ASignal
+    {
+        public event Action<T> Listener = null;
+        public event Action<T> OnceListener = null;
+
+        public void AddListener(Action<T> callback) { Listener = this.AddUnique(Listener, callback); }
+
+        public void AddOnce(Action<T> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+
+        public void RemoveListener(Action<T> callback)
+        {
+            if (Listener != null)
+                Listener -= callback;
+        }
+
+        public void RemoveOnceListener(Action<T> callback)
+        {
+            if (OnceListener != null)
+                OnceListener -= callback;
+        }
+
+        public override List<Type> GetTypes()
+        {
+            List<Type> retv = new List<Type>();
+            retv.Add(typeof(T));
+            return retv;
+        }
+
+        public void Dispatch(T type1)
+        {
+            Listener?.Invoke(type1);
+            OnceListener?.Invoke(type1);
+            OnceListener = null;
+            object[] outv = {type1};
+            base.Dispatch(outv);
+        }
+
+        private Action<T> AddUnique(Action<T> listeners, Action<T> callback)
+        {
+            if (listeners == null || !listeners.GetInvocationList().Contains(callback))
+            {
+                listeners += callback;
+            }
+
+            return listeners;
+        }
+
+        public override void RemoveAllListeners()
+        {
+            Listener = null;
+            OnceListener = null;
+            base.RemoveAllListeners();
+        }
+
+        public Delegate listener { get { return Listener ?? (Listener = delegate { }); } set { Listener = (Action<T>) value; } }
+    }
+}
