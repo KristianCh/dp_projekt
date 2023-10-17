0b7e408dc2b9fa3952fa1aa7bb4eb4d0e95a1c5c
Kristián Chovančák <chovancak.kristian@gmail.com>
2023-10-17 20:46:34 +0200

    added basic world object movement controller

---
+++ Assets/Entities/Gameplay/WorldObjectMovementController.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Utils;
+using UnityEngine;
+
+namespace Entities.Gameplay
+{
+    public class WorldObjectMovementController : MonoBehaviour
+    {
+        [SerializeField] 
+        private float _MinZ = -20;
+        public void Update()
+        {
+            if (transform.position.z < _MinZ)
+            {
+                transform.position = transform.position.WithZ(transform.position.z - Time.deltaTime);
+            }
+        }
+    }
+}
