f793eac400b0efaf95b1a0b4cc9bc9a47e3e9ff6
Kristián Chovančák <chovancak.kristian@gmail.com>
2024-05-04 16:38:24 +0200

    Leaderboard text fixes

--- Assets/Entities/DataManagement/HighscoreItem.cs
+++ Assets/Entities/DataManagement/HighscoreItem.cs
@@ -5,9 +5,6 @@ namespace Entities.DataManagement
 {
     public class HighscoreItem : MonoBehaviour
     {
-        [SerializeField]
-        private TMP_Text _PositionText;
-        
         [SerializeField] 
         private TMP_Text _NickText;
         
@@ -16,8 +13,7 @@ namespace Entities.DataManagement
 
         public void SetData(int position, string nickname, float score, bool isPlayer)
         {
-            _PositionText.text = position + ".";
-            _NickText.text = nickname;
+            _NickText.text = position + ". " + nickname;
             _ScoreText.text = Mathf.RoundToInt(score).ToString();
             _NickText.color = isPlayer ? Color.green : Color.white;
         }
