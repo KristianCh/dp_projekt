37e03d73a411ec4e9bd3e9bafb07317726493613
Kristián Chovančák <chovancak.kristian@gmail.com>
2024-05-07 19:00:49 +0200

    applied feedback

--- Assets/Entities/DataManagement/HighscoreItem.cs
+++ Assets/Entities/DataManagement/HighscoreItem.cs
@@ -13,7 +13,7 @@ namespace Entities.DataManagement
 
         public void SetData(int position, string nickname, float score, bool isPlayer)
         {
-            _NickText.text = position + ". " + nickname;
+            _NickText.text = position + ". " + nickname + ":";
             _ScoreText.text = Mathf.RoundToInt(score).ToString();
             _NickText.color = isPlayer ? Color.green : Color.white;
         }
